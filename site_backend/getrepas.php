1eb340dcb6683f385496e5057cee5f4af69bd343
spaghefoo <sofiane59660@gmail.com>
2022-03-18 21:49:23 +0100

    Rajouté une fonction pour le SQL dans fonctions.php que j'utilise désormais dans reserv.php(et bientot getrepas je pense), exampleaffichagerepas dans site backend est un exemple d'intégration des repas dans le site uniquement en js.

--- site_backend/getrepas.php
+++ site_backend/getrepas.php
@@ -1,39 +1,43 @@
 <?php
-header('Content-TYpe: application/json; charset=utf-8');
-$i = 0;
+
+//HEADER OF THE FILE(THIS FILE RETURN JSON SO APPLICATION/JSON)
+header('Content-Type: application/json; charset=utf-8');
 require('../Panel_Admin/database.php');
 
+// WE DEFINE SOME VARIABLES
+$i = 0;
+$array = array();
 $mois = date('m');
-
 $sql = "SELECT * FROM platdujour WHERE numMois = ? ORDER BY prixHT DESC";
 
 
-if($bigprep = mysqli_prepare($db, $sql));
+
+
+//MAIN CODE
+
+//Nouvelle fonction 
+if($bigprep = mysqli_prepare($db, $sql));//PREPARED STATEMENT 
 {
-  mysqli_stmt_bind_param($bigprep, 'i', $mois);
+  mysqli_stmt_bind_param($bigprep, 'i', $mois);// WE APPLY THE VARIABLE $moisINTO THE SQL QUERY.
 }
 
-mysqli_stmt_execute($bigprep);
-$query = mysqli_stmt_get_result($bigprep);
+mysqli_stmt_execute($bigprep);// WE EXECUTE THE QUERY
+$query = mysqli_stmt_get_result($bigprep);//WE GET THE RESULT IN ORDER TO FETCH A ASSOCIATIVE ARRAY.
 
-$array = array();
     while($fetch = mysqli_fetch_assoc($query))
     {
-      
         $array['nomPlat'][$i] = $fetch['nomPlatJour'];
-        $array['prixHT'][$i] = $fetch['prixHT'];
-        $i++;
+        $array['prixHT'][$i] = $fetch['prixHT']; 
+        // PUSHIN INTO THE ARRAY $array THE DISHES AND THEIR TAX LESS PRICE.(english)
+        $i++;//2D ARRAY
     }
 
 
   
 
-
+// WE ENCODE INTO JSON(application/json);
 echo json_encode($array);
-
-
+// THEN WE CLOSE THE CONNECTION TO THE DATABASEEEEEE
 mysqli_stmt_close($bigprep);
-
-
 mysqli_close($db);
 ?>
--- site_backend/reserv.php
+++ site_backend/reserv.php
@@ -17,7 +17,6 @@ if(!empty($_POST))
     $datepart = explode("T", $datefull);
     $dateint = intval($datepart[1]);
 	
-    $reqtype = 0;
 	
     if($dateint >= 12 AND $dateint < 18)
     {
@@ -29,46 +28,13 @@ if(!empty($_POST))
     }
 
 
-
-
-    $query = mysqli_query($db,"SELECT id_Reservation, mail, telephone FROM reservations");
-
+    $array = array($prenom_client, $nom_client, $tel_client, $email_client, $datefull, $nbre_personne, $service);
+    //$query = mysqli_query($db,"SELECT id_Reservation, mail, telephone FROM reservations");
+    
     $sql = "INSERT INTO reservations(prenom, nom, telephone, mail, date_Reservation, nbr_Personnes, service) VALUES(?, ?, ?, ?, ?, ?, ?)";
 
-    while($data = mysqli_fetch_assoc($query))
-    {
-        if(isset($_POST['update']))
-        {
-    	   if($data['mail'] == $email_client OR $data['telephone'] == $tel_client)
-    	{
-            $email_content = file_get_contents('email/email.html');
-            
-    		$sql = "UPDATE reservations SET nbr_Personnes = ?, date_Reservation = ?, service = ? WHERE id_Reservation = ?";
-            $reqtype = 1;
-    		break;
-    	}
-      }
-    }
 
-
-    if($bigprep = mysqli_prepare($db, $sql))
-    {
-        if($reqtype == 0)
-        {
-            mysqli_stmt_bind_param($bigprep, 'ssissis', $prenom_client, $nom_client, $tel_client, $email_client, $datefull, $nbre_personne, $service);
-        }
-        else
-        {  
-            mysqli_stmt_bind_param($bigprep, 'issi', $nbre_personne, $datefull, $service, $data['id_Reservation']);
-        }
-    }
-
- 	//mail($data['mail'], "Votre reservation est validé!", $email_content, "From: sofiane59660@hotmail.fr");
-
-    mysqli_stmt_execute($bigprep);
-    mysqli_stmt_close($bigprep);
-
-    echo mysqli_error($db);
+    $result = masseSQL($db, $sql, 'ssissis', $array);
 
     mysqli_close($db);
 
@@ -85,7 +51,6 @@ if(!empty($_POST))
     print_r(CountAllReserv($datefull,$service));
 */
 
-//    header("Location: test.html");
   }
 
 else
